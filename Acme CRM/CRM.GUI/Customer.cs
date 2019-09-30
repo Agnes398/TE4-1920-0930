71792ddddcee6c8f264736e73f29b130483f787d
Agnes Löjdkvist <agnes.lojdkvist@edu.huddinge.se>
2019-09-30 08:49:29 +0200

    created project + class customer

---
+++ Acme CRM/CRM.GUI/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRM.GUI
+{
+    public class Customer
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool VIP { get; set; }
+        
+    }
+}
