e07cc4226893e9583ef471a5969ed1c729cb4a12
Agnes Löjdkvist <agnes.lojdkvist@edu.huddinge.se>
2019-09-30 09:02:49 +0200

    button code to add customer

--- Acme CRM/CRM.GUI/Form1.cs
+++ Acme CRM/CRM.GUI/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,30 @@ namespace CRM.GUI
 {
     public partial class Form1 : Form
     {
+        ArrayList MyCustomers = new ArrayList();
         public Form1()
         {
             InitializeComponent();
+
+            button1.Text = "Spara kund";
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Spara kund
+            //Var ska kunden sparas?
+            //repository = "sparenhet" (ex: DB, fil, array etc)
+            //vi vill göra det enkelt, vi väljer en ArrayList
+
+            MyCustomers.Add(new Customer() { FirstName = textBox1.Text, LastName = textBox2.Text });
+
+            //visa för användaren att kunden är sparad
+
+            textBox1.Text = "";
+            textBox2.Text = string.Empty; //detta tar ingen plats och är en logisk representation av tom sträng
+
+
         }
     }
 }
